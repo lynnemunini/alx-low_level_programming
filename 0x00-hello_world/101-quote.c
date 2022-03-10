defb0f5e440777fe558cb515a27bea79f8905ff0
lynnemunini <muninilynne65@gmail.com>
2022-03-10 06:00:09 -0800

    Added 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
   * main - prints a string to standard error
   * Return: program returns 1
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
