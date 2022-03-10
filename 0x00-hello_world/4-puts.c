76467b4d8371a0cad759b9464aa1e51308defe8c
lynnemunini <muninilynne65@gmail.com>
2022-03-10 03:55:39 -0800

    Changed main.c header file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include stdio.h
+#include <stdio.h>
 /**
   * main - Outputs a string
   * Return: 0 returned to exit the program
   */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
