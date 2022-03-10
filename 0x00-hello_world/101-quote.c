d77acbfad9da0c3e49a4b070cbfbc97ef7d71081
lynnemunini <muninilynne65@gmail.com>
2022-03-10 05:47:13 -0800

    Added 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,6 +5,6 @@
  */
 int main(void)
 {
-	stderr("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 	return (1);
 }
