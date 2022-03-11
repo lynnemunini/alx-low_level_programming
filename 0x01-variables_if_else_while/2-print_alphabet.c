4e0baccc0f19e0e02293b9a52e1667cd09fa2679
lynnemunini <muninilynne65@gmail.com>
2022-03-11 04:54:15 -0800

    Added 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  * main - program that prints the alphabet in lowercase
+  * Return: 0 exit code of the program if successful
+ */
+int main(void)
+{
+	char alphabet;
+	alphabet = a;
+	while(alphabet < = "z")
+	{
+		putchar(alphabet);
+		putchar("\n");
+		alphabet++;
+	}
+	return (0);
+}
+
