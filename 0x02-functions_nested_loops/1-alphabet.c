3dbebc5559f9193fd90a29a238a82d46e1974d77
lynnemunini <muninilynne65@gmail.com>
2022-03-15 06:36:47 -0700

    Removed main.h

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - prints the alphabet, in lowercase, followed by a new line.
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	char aphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		_putchar(alphabet);
+	}
+	_putchar('\n');
+}
+	
+
