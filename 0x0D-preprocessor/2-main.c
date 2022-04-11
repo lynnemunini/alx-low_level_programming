102577449d377d4fbbe1c2eeca5f7f5199eb8b1b
lynnemunini <muninilynne65@gmail.com>
2022-04-11 07:48:51 -0700

    Added description for return value

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * main - prints the name of the file it was compiled from, followed by a new line
- * RETURN: 0 if program runs successfully
+ * main - prints the name of the file it was compiled from
+ * followed by a new line
+ * Return: 0 if program runs successfully
  */
 int main(void)
 {
