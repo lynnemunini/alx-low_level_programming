eccc73866fbc7d13d99f80a2422b705520f81005
lynnemunini <muninilynne65@gmail.com>
2022-04-11 07:42:46 -0700

    Added 2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints the name of the file it was compiled from, followed by a new line
+ * RETURN: 0 if program runs successfully
+ */
+int main(void)
+{
+	_putchar(__FILE__);
+	_putchar('\n');
+	return (0);
+}
